67cba83c67eda4faee1b8045a6e78638abbeefff
Kamil Pilkiewicz <92442879+KamilloP@users.noreply.github.com>
2025-06-19 12:56:22 +0200

    Standing in proper folder

--- examples/standing.py
+++ examples/standing.py
@@ -1,34 +1,17 @@
 # mypy: disable-error-code="override"
-"""Defines simple task for training a jumping policy for the default humanoid using an GRU actor."""
+"""Defines simple task for training a standing policy for the default humanoid using an GRU actor."""
 
 from dataclasses import dataclass
 from typing import Generic, TypeVar
-
+import xax
 import ksim
-
-from .walking import HumanoidWalkingTask, HumanoidWalkingTaskConfig
-
-
-@dataclass
-class HumanoidStandingTaskConfig(HumanoidWalkingTaskConfig):
-    pass
-
-
-Config = TypeVar("Config", bound=HumanoidStandingTaskConfig)
-
-
-class HumanoidStandingTask(HumanoidWalkingTask[Config], Generic[Config]):
-    def get_rewards(self, physics_model: ksim.PhysicsModel) -> list[ksim.Reward]:
-        return [
-            ksim.BaseHeightRangeReward(z_lower=1.1, z_upper=1.5, dropoff=10.0, scale=1.0),
-            ksim.StayAliveReward(scale=1.0),
-        ]
-
+from .rewards.standing_reward import HumanoidStandingTaskConfig, HumanoidStandingTask
 
 if __name__ == "__main__":
     # To run training, use the following command:
     #   python -m examples.standing
     # To visualize the environment, use the following command:
+    #  python -m examples.standing run_mode=view batch_size=1 num_envs=1
     #   python -m examples.standing run_mode=view
     HumanoidStandingTask.launch(
         HumanoidStandingTaskConfig(
